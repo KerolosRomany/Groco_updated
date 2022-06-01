ca6a9b557f586e9c3978a6d066f4f568036f2e2f
Kerolos Romany <73957933+KerolosRomany@users.noreply.github.com>
2022-06-01 19:52:17 +0200

    Add files via upload

---
+++ dashboard/brand/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from dashboard.decorators import permession
+
+
+app_name='brand_dashboard'
+
+urlpatterns = [
+    path('create/', permession(views.CreateBrand.as_view()), name = 'create_brand'),
+    path('update/<int:pk>/', permession(views.UpdateView.as_view()), name = 'update_brand'),
+    path('list/', permession(views.ListBrand.as_view()), name = 'list_brand'),
+]
