4f81ea62491dd78e3ffadd20b7135b03cc348fd1
Youssef Nour <yousnour222@gmail.com>
2024-07-19 15:13:27 +0300

    Update settings.py

--- Groco/settings.py
+++ Groco/settings.py
@@ -156,7 +156,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'grocoshop@gmail.com'
-EMAIL_HOST_PASSWORD = 'youssef nour1'
+EMAIL_HOST_PASSWORD = 'dcyd zyjm ngkz whht'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
